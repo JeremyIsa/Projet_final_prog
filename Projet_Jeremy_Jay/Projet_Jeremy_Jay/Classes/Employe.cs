a4dd6ee802257e0bb1c86a2d3300cd799944f5fd
Spinyomnivore <charette2004.2@gmail.com>
2025-11-24 14:25:37 -0500

    cour 24 novembre
    cour 24 novembre

--- Projet_Jeremy_Jay/Projet_Jeremy_Jay/Classes/Employe.cs
+++ Projet_Jeremy_Jay/Projet_Jeremy_Jay/Classes/Employe.cs
@@ -8,5 +8,46 @@ namespace Projet_Jeremy_Jay.Classes
 {
     class Employe
     {
+        string matricule;
+        string num_projet;
+        string prenom;
+        string nom;
+        DateTime date_naissance;
+        string email;
+        string adresse;
+        DateTime date_embauche;
+        double taux_horaire;
+        string photo_id;
+        string status;
+
+        public Employe(string matricule, string num_projet, string prenom, string nom, DateTime date_naissance, string email, string adresse, DateTime date_embauche, double taux_horaire, string photo_id, string status)
+        {
+            this.matricule = matricule;
+            this.num_projet = num_projet;
+            this.prenom = prenom;
+            this.nom = nom;
+            this.date_naissance = date_naissance;
+            this.email = email;
+            this.adresse = adresse;
+            this.date_embauche = date_embauche;
+            this.taux_horaire = taux_horaire;
+            this.photo_id = photo_id;
+            this.status = status;
+        }
+
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public DateTime Date_naissance { get => date_naissance; set => date_naissance = value; }
+        public string Email { get => email; set => email = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
+        public DateTime Date_embauche { get => date_embauche; set => date_embauche = value; }
+        public double Taux_horaire { get => taux_horaire; set => taux_horaire = value; }
+        public string Photo_id { get => photo_id; set => photo_id = value; }
+        public string Status { get => status; set => status = value; }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
     }
 }
